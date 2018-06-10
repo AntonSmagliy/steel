8e63767067e6d86e6bd5c7e67a4ab2bbb2d065fa
Mazh Vlad <mazhor02@gmail.com>
2018-06-11 00:58:10 +0300

    first_parallax

--- js/common.js
+++ js/common.js
@@ -13,4 +13,38 @@ $(function(){
         prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
         nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
 	});
+	if($(window).width() > 1024)
+{
+
+   $('body').parallax({
+
+      'elements': [
+        {
+          'selector': '.paralax_block',
+          'properties': {
+            'x': {
+              'right': {
+                'initial': 150,
+                'multiplier': 0.04,
+                'unit': 'px',
+                'invert': false
+              }
+            },
+            'y': {
+              'top': {
+                'initial': 54,
+                'multiplier': 0.02,
+                'unit': 'px',
+                'invert': false
+              }
+            }
+          }
+        }
+      ]
+    });
+
+} else {
+   // change functionality for larger screens
+}
+
 });
