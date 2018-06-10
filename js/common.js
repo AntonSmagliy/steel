01f53310b4a3e3f13182031d76bcc4928f49b127
Mazh Vlad <mazhor02@gmail.com>
2018-06-10 17:51:06 +0300

    a lot

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.offer_reviews').slick({
+		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		arrows: true,
+        prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+	});
+});
